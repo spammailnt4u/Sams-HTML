b4c21d2f42596342e0c85ce0d5837bd0a6e11385
spammailnt4u <spammailnt4u@gmail.com>
2016-03-03 11:36:51 -0500

    Added files from practice project

---
+++ Chapter_4/javascript.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+  now=new Date();
+  localtime=now.toString();
+  utctime=now.toGMTString();
+  document.write("<p><strong>Local time:</strong>"+localtime+"</p>");
+  document.write("<p><strong>UTC time:</strong>"+utctime+"</p>");
+</script>
